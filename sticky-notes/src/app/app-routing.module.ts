c7c4cb0214eaabc951c28c4bf10aa73ac47d3ac0
FabianCalvoAlcazar <fabiianca16@gmail.com>
2024-01-06 22:55:50 -0600

    Home and Error page added with styles and functionallity

--- sticky-notes/src/app/app-routing.module.ts
+++ sticky-notes/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-    {path: "", component: LoginComponent},
+    {path: "", component: HomeComponent},
+    {path: "home", component: HomeComponent},
     {path: "login", component: LoginComponent},
     {path: "register", component: RegisterComponent},
     {path: '**', component: ErrorComponent}
