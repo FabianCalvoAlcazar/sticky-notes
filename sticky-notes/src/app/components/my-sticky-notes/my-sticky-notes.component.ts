e784c1a6a97aa0432cb4c5b62e202b4369f1a9e1
FabianCalvoAlcazar <fabiianca16@gmail.com>
2024-01-21 20:13:16 -0600

    Add functionallity done

--- sticky-notes/src/app/components/my-sticky-notes/my-sticky-notes.component.ts
+++ sticky-notes/src/app/components/my-sticky-notes/my-sticky-notes.component.ts
@@ -5,7 +5,8 @@ import { Dialog, DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
 import { StickyNoteService } from 'src/app/services/stickyNote.service';
 import { FormsModule } from '@angular/forms';
 import { EditorModule } from '@tinymce/tinymce-angular';
-import { STRING_TYPE } from '@angular/compiler';
+import { StickyNoteModel } from 'src/app/models/stickyNote';
+import { Router } from '@angular/router';
 
 // Used by component add-sticky-notes
 export interface DialogData {
@@ -23,16 +24,13 @@ export class MyStickyNotesComponent implements OnInit{
   public user:any;
   public stickyNotes:Array<any>
   public thereAreNotes: boolean;
+  public newStickyNote!: StickyNoteModel;
 
-  public newStickyNote= {
-    newNoteTitle: "",
-    newNoteText: ""
-  }
-
-  constructor(private _stickyNoteService:StickyNoteService, public dialog: Dialog){
+  constructor(private _stickyNoteService:StickyNoteService, public _dialog: Dialog, private _router:Router){
     this.user = "";
     this.stickyNotes = [];
     this.thereAreNotes = false;
+    this.newStickyNote = new StickyNoteModel("","","",0,0)
     this.getStickyNotes();
   }
 
@@ -40,7 +38,9 @@ export class MyStickyNotesComponent implements OnInit{
   }
 
   getStickyNotes(){
-    this._stickyNoteService.getStickyNotes().subscribe(
+    var userId = localStorage.getItem("userId");
+
+    this._stickyNoteService.getStickyNotes(userId).subscribe(
       response => {
         if (response) {
           this.user = response.user;
@@ -60,25 +60,34 @@ export class MyStickyNotesComponent implements OnInit{
   }
 
   async addStickyNote(){
-    const dialogRef = this.dialog.open<any>(AddStickyNoteComponent, {
-      width: '1000px',
-      data: {
-        newNoteTitle: String, 
-        newNoteText: String
-      }
+    const dialogRef = this._dialog.open<any>(AddStickyNoteComponent, {
+      width: 'auto'
     });
 
     dialogRef.closed.subscribe(result => {
       if (result) {
         if (result.newNoteTitle == ''){
-          this.newStickyNote.newNoteTitle = "New Note";
-          this.newStickyNote.newNoteText =  result.newNoteText;
-          console.log(this.newStickyNote)
+          this.newStickyNote.title = "New Note";
+          this.newStickyNote.text =  result.newNoteText;
         } else {
-          this.newStickyNote.newNoteTitle = result.newNoteTitle;
-          this.newStickyNote.newNoteText= result.newNoteText;
-          console.log(this.newStickyNote)
+          this.newStickyNote.title = result.newNoteTitle;
+          this.newStickyNote.text= result.newNoteText;
         }
+
+        this.newStickyNote.owner = localStorage.getItem("userId");
+
+        this._stickyNoteService.saveStickyNote(this.newStickyNote).subscribe(
+          response => {
+            console.log(response.message)
+            const currentUrl = this._router.url;
+            this._router.navigateByUrl('/empty', { skipLocationChange: true }).then(() => {
+              this._router.navigate([currentUrl])
+            });
+          },
+          error => {
+            console.log("Error: ", error)
+          }
+        );
       } else {
         console.log('canceled')
       }
--- sticky-notes/src/app/models/stickyNote.ts
+++ sticky-notes/src/app/models/stickyNote.ts
@@ -1,5 +1,6 @@
 export class StickyNoteModel{
     constructor(
+        public owner:string | null,
         public title:string,
         public text:string,
         public positionX:number,
